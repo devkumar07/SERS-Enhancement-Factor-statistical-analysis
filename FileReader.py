620a8d843f5b9b7736fe6b6fa3c8617274feed37
Devanshu Kumar <devanshu.kumar07@gmail.com>
2019-12-27 22:53:15 -0800

    Added script and packages for reading data from VTK

--- FileReader.py
+++ FileReader.py
@@ -1,24 +1,11 @@
 import numpy as np
-import pandas as pd
+import pyvista
+data = pyvista.read('Data/ElectricField3DField.vtk')
 
-
-def fetch_data(dx,dy,dz,file):
-    data = pd.read_excel(file)
-    x = np.zeros((dx,dy,dz), dtype= float)
-    col = 0
-    row = 0
-    for z in range(0,dz):
-        for y in range(0,dy):
-            for i in range(0,dx):
-                x[i][y][z] = data.iloc[row][col]
-                col = col + 1
-                if col == x:
-                    row = row + 1
-                    col = 0
-
-    for z in range(0,dz):
-        print("z= 0")
-        for y in range(0,dy):
-            for i in range(0,dx):
-                print(x[i][y][z])
-            
+# Get points as NumPy array
+print(data['-0.0005 -0.0005 -0.0005'])
+points = data.points
+print(points)
+print(data.field_arrays)
+print(data.point_arrays['Intensity'])
+print(data.plot(show_grid=True))

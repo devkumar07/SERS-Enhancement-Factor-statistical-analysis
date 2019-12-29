4293e733d1dade1f770f547556be978bd7e084e7
Devanshu Kumar <devanshu.kumar07@gmail.com>
2019-12-28 21:38:48 -0800

    Added radius_threshold function

--- EFCalculation.py
+++ EFCalculation.py
@@ -1,6 +1,7 @@
 #This script performs essential statistical analysis
 import numpy as np 
 import pyvista
+import math
 
 def get_x_coordinate(coordinate_vector, index):
     return coordinate_vector[index][0]
@@ -10,3 +11,10 @@ def get_y_coordinate(coordinate_vector, index):
 
 def get_z_coordinate(coordinate_vector, index):
     return coordinate_vector[index][2]
+
+def radius_threshold(x,y,radius):
+    r = math.floor(math.sqrt(x**2 + y**2))
+    if r == radius:
+        return True
+    return False
+
